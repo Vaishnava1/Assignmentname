13a5f6c28ea401da45114d54712821f7a47d66c1
Vaishnava1 <127789553+Vaishnava1@users.noreply.github.com>
2023-03-20 23:21:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+import { createNoise2D } from "simplex-noise";
+
+const noise = createNoise2D;
+const arr = new Array(101)
+  .fill(0)
+  .map((d, i) => i)
+  .filter((d, i) => d % 2 === 0);
+const date = new Date();
+
+const dates = arr
+  .map((d) => date.getTime() - d * 24 * 60 * 60 * 1000)
+  .map((d) => new Date(d));
+const randomValues = dates.map((date) => ({
+  date,
+  val: date
+}));
+function makeNoise(d, randomValues) {
+  return 200 + noise(randomValues * d);
+}
+export default function App() {
+  return (
+    <div className="App">
+      {randomValues.map((randomValues, index) => (
+        <div
+          key={index}
+          className="dot"
+          style={{
+            left: `${index * 13 + 3}px`,
+            transform: `translateY(-${randomValues.val}px)`
+          }}
+        />
+      ))}
+    </div>
+  );
+}
